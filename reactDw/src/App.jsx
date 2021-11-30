d63063b51b7703f178af03c3f559fa905845cec0
Pedrofreitas18 <opedrofreitas@gmail.com>
2021-11-30 21:22:32 -0200

    Atividade react

---
+++ reactDw/src/App.jsx
@@ -0,0 +1,6 @@
+import './styles/global.scss'
+
+export function App(){
+    //throw new Error("Erro lançado")
+    return <h2>Hello World</h2>
+}
---
+++ reactDw/src/index.jsx
@@ -0,0 +1,5 @@
+//import React from 'react';
+import {render} from 'react-dom'
+import {App} from './App' 
+
+render(<h1>Helllo World 2</h1>, document.getElementById('root'))
